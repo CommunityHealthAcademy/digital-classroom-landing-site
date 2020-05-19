2795873c38b824cf7f62ec2e7fdc5c68fd47c28a
rachelle ragasa <rachelleragasa.costa@gmail.com>
2020-05-19 23:08:36 +0100

    build(setup): adding css variables and commitlint

---
+++ src/styles/breakpoints.js
@@ -0,0 +1,24 @@
+import { css } from "styled-components"
+
+export const breakpoint = {
+  mobile: "375px",
+  desktop: "1440px",
+}
+
+export const above = Object.keys(breakpoint).reduce((acc, name) => {
+  acc[name] = (...args) => css`
+    @media (min-width: ${breakpoint[name]}) {
+      ${css(...args)}
+    }
+  `
+  return acc
+}, {})
+
+export const below = Object.keys(breakpoint).reduce((acc, name) => {
+  acc[name] = (...args) => css`
+    @media (max-width: calc(${breakpoint[name]} - 1px)) {
+      ${css(...args)}
+    }
+  `
+  return acc
+}, {})
---
+++ src/styles/color.js
@@ -0,0 +1,10 @@
+export const color = {
+  red: "#ee3d23",
+  black: "#000000",
+  white: "#ffffff",
+  teal: "#84b9b6",
+  green: "#9fc65b",
+  blue: "#3d68ad",
+  pink: "#ee8cb7",
+  darkBlue: "#164780",
+}
